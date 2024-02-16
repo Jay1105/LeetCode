bf2aa1a6076b41973373331b08cf4cdd79333067
Jay Chotalia <75968475+Jay1105@users.noreply.github.com>
2024-02-16 15:27:05 +0530

    Time: 12 ms (67.28%), Space: 19 MB (18.02%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) 
+    {
+        for (int i = 1; i < nums.size(); i++)
+            nums[i] = max(i + nums[i], nums[i - 1]);
+        int idx = 0;
+        int ans = 0;
+        
+        while (idx < nums.size() - 1)
+        {
+            ans++;
+            idx = nums[idx];
+        }
+        return ans;
+    }
+};
