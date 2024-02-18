32504d04cbd2610342696b3f27bfcddd95783ab8
Jay Chotalia <75968475+Jay1105@users.noreply.github.com>
2024-02-18 17:46:12 +0530

    Time: 15 ms (65.89%), Space: 19.5 MB (44.20%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -4,30 +4,29 @@ public:
         int n = ratings.size();
         int res = n;
         
-        int icnt = 1, dcnt = 0;
+        int up = 0, down = 0, peak = 0;
         for (int i = 1; i < n; i++)
         {
             if (ratings[i] > ratings[i - 1])
             {
-                if (i - 2 >= 0 && ratings[i - 1] < ratings[i - 2])
-                    icnt = 1;
-                res += icnt;
-                icnt += 1;
-                dcnt = 0;
+                down = 0;
+                up += 1;
+                res += up;
+                peak = up;
             }
             else if (ratings[i] == ratings[i - 1])
             {
-                icnt = 1;
-                dcnt = 0;
+                up = 0;
+                down = 0;
+                peak = 0;
             }
             else
             {
-                if (icnt > 1)
-                    icnt--;
-                else
-                    res += 1;
-                res += dcnt;
-                dcnt += 1;
+                up = 0;
+                down += 1;
+                res += down;
+                if (peak >= down)
+                    res--;
             }
         }
         return res;
