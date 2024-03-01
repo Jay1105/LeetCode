7bf483f35474ca79d8c4c8c58920e68425398f86
Jay Chotalia <75968475+Jay1105@users.noreply.github.com>
2024-03-01 13:37:08 +0530

    Time: 3 ms (39.85%), Space: 7.9 MB (13.34%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        vector<string> arr;
+        string temp = "";
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == ' ')
+            {
+                arr.push_back(temp);
+                temp = "";
+            }
+            else
+                temp += s[i];
+        }
+        arr.push_back(temp);
+        
+        map<char, string> mp;
+        map<char, string>:: iterator it;
+        
+        if (pattern.size() != arr.size())
+            return false;
+                
+        for (int i = 0; i < pattern.size(); i++)
+        {
+            string tt = arr[i];
+
+            if (mp[pattern[i]] == "")
+            {
+                for (it = mp.begin(); it != mp.end(); it++)
+                {
+                    if (it->second == tt)
+                        return false;
+                }
+                mp[pattern[i]] = tt;
+            }
+            else if (mp[pattern[i]] != tt)
+                return false;
+        }
+        return true;
+    }
+};
