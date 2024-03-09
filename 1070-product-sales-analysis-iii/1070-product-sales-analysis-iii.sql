7143aaa6625013567961c9d8740b7cb7c7418b16
Jay Chotalia <75968475+Jay1105@users.noreply.github.com>
2024-03-09 11:25:46 +0530

    Delete 1070-product-sales-analysis-iii directory

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++
@@ -1,5 +0,0 @@
-# Write your MySQL query statement below
-select a.product_id, a.year as first_year, a.quantity, a.price
-from Sales a
-where (a.product_id, a.year)
-in (select product_id, min(year) from Sales group by product_id);
