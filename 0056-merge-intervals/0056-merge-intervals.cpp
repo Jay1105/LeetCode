b81d535a62cdcd49ca8fe4d7e89ae4f97048a1c1
Jay Chotalia <75968475+Jay1105@users.noreply.github.com>
2024-03-11 11:02:12 +0530

    Time: 114 ms (8.72%), Space: 51.6 MB (5.01%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int n = intervals.size();
-        int m = intervals[0].size();
-        
         sort(intervals.begin(), intervals.end(), [&](vector<int> a, vector<int> b)
              {
                  return a[0] < b[0];
              });
         
         vector<vector<int>> ans;
-        vector<int> temp;
-        
         int idx1 = intervals[0][0], idx2 = intervals[0][1];
         
-        for (int i = 1; i < n; i++)
+        for (int i = 1; i < intervals.size(); i++)
         {
             if (idx2 >= intervals[i][0] && intervals[i][1] >= idx1)
             {
@@ -23,17 +18,11 @@ public:
             }
             else
             {
-                temp.push_back(idx1);
-                temp.push_back(idx2);
-                ans.push_back(temp);
-                temp.clear();
-                
+                ans.insert(ans.end(), {idx1, idx2});                
                 idx1 = intervals[i][0], idx2 = intervals[i][1];
             }
         }
-        temp.push_back(idx1);
-        temp.push_back(idx2);
-        ans.push_back(temp);
+        ans.insert(ans.end(), {idx1, idx2});    
         
         return ans;
     }
