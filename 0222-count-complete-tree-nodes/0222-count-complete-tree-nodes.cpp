fc44bab18685bdf786a4b0d7af0e7b0a36bfdb0f
Jay Chotalia <75968475+Jay1105@users.noreply.github.com>
2024-04-15 13:56:13 +0530

    Time: 23 ms (63.33%), Space: 29.1 MB (78.93%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -14,6 +14,8 @@ public:
     int countNodes(TreeNode* root) {
         if (root == NULL)
             return 0;
+        if (root->left == NULL && root->right == NULL)
+            return 1;
         return countNodes(root->left) + countNodes(root->right) + 1;
     }
 };
