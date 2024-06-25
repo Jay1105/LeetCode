8a0939de9547e4114e49f3e2e1c73c2062aaa9b6
Jay Chotalia <75968475+Jay1105@users.noreply.github.com>
2024-06-25 22:58:47 +0530

    Time: 15 ms (84.11%), Space: 26.2 MB (44.60%) - LeetHub

---
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    long mx = INT_MIN;
+    
+    long helper(TreeNode* root) {
+        if (root == NULL)
+            return INT_MIN;
+        
+        long left = helper(root->left);
+        long right = helper(root->right);
+        
+        long closed = left + right + root->val;
+        long path = root->val + max(left, right);
+        
+        mx = max(max((long)root->val, mx), max(closed, path));
+        
+        return max((long)root->val, path);
+    }
+
+    long maxPathSum(TreeNode* root) {
+        long tp = helper(root);
+        
+        return mx;
+    }
+};
