9388d61105c63303fc8e7d32ff711e3344494d16
Jay Chotalia <75968475+Jay1105@users.noreply.github.com>
2024-07-21 00:46:20 +0530

    Time: 14 ms (56.46%), Space: 15.9 MB (68.02%) - LeetHub

---
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    pair<int, int> getPos(int cell, int n)
+    {
+        int row = (cell - 1) / n;
+        int col = (cell - 1) % n;
+        
+        if (row & 1)
+            col = n - 1 - col;
+        row = n - 1 - row;
+        
+        return make_pair(row, col);
+    }
+    
+    int snakesAndLadders(vector<vector<int>>& board) {
+        int n = board.size();
+        
+        queue<int> q;
+        vector<int> dist(n*n + 1, -1);
+        dist[1] = 0;
+        
+        q.push(1);
+        
+        while (!q.empty())
+        {
+            int cell = q.front();
+            q.pop();
+            
+            for (int i = cell + 1; i <= min(cell + 6, n*n); i++)
+            {
+                pair<int, int> pos = getPos(i, n);
+                
+                int dest = board[pos.first][pos.second] != -1 ? board[pos.first][pos.second] : i;
+                
+                if (dist[dest] == -1) {
+                    dist[dest] = dist[cell] + 1;
+                    q.push(dest);
+                }
+            }
+        }
+        
+        return dist[n*n];
+    }
+};
